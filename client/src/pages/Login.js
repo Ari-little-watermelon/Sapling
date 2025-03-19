7e09af70c23c7a152bbc296b076131978fd186b9
Arundhati Bandopadhyaya <bandopadhyaya.a@northeastern.edu>
2025-03-18 22:04:12 -0400

    login/signup base

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,20 +3,18 @@ import SignupInput from "../components/SignupInput";
 import SignupButton from "../components/SignupButton";
 import { Link } from 'react-router-dom';
 
-function Signup() {
+function Login() {
     return (
         <div className="Signup">
             <div className="signup-inner">
-                <h1>Signup</h1>
-                <p>Already have an account? <Link to="/">Login</Link></p>
-                <SignupInput title="Full Name"/>
+                <h1>Login</h1>
+                <p>Don't have an account? <Link to="/signup">Sign up</Link></p>
                 <SignupInput title="Email Address"/>
-                <SignupInput title="Create Password"/>
-                <SignupInput title="Confirm Password"/>
-                <SignupButton title="Sign Up"/>
+                <SignupInput title="Password"/>
+                <SignupButton title="Login"/>
             </div>
         </div>
     )
 }
 
-export default Signup;
+export default Login;
